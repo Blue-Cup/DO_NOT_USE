7c045f5d9114f523225d168b01fd75b06e5e423b
Matthew Hirth <hirth.24@osu.edu>
2022-03-04 14:25:34 -0500

    Navigation update 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
-import Product from './Products/Products';
+import Products from './Products/Products';
 import Home from './Home/Home';
 
 function App() {
@@ -16,45 +16,45 @@ function App() {
 
   return (
     <Router>
-      <div className="grid-container">
+    <div className="grid-container">
+      <head>
         <link rel="stylesheet" href="style.css" />
         <title>Blue Cup</title>
+      </head>
+      <body>
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <a href="index.htm">Blue Cup</a>
+          </div>
+          <div className="header-links">
+              <Link to="/">Home</Link>
+              <Link to="/catalog">Catalog</Link>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <ul>
+            <li>
+              <a href="index.html">Pants</a>
+            </li>
 
-        <body>
-          <div>
-            <header className="header">
-              <div className="brand">
-                <button onClick={openMenu}>&#9776;</button>
-                <a href="index.htm">Blue Cup</a>
-              </div>
-              <div className="header-links">
-                <Link to="/">Home</Link>
-                <Link to="/">Catalog</Link>
-              </div>
-            </header>
-            <aside className="sidebar">
-              <h3>Shopping Categories</h3>
-              <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-              <ul>
-                <li>
-                  <a href="index.html">Pants</a>
-                </li>
-
-                <li>
-                  <a href="index.html">Shirts</a>
-                </li>
-              </ul>
-            </aside>
-            <main className="main">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/catalog" element={< { Products />}} />
-              </Routes>
-            </main>
-            <footer className="footer">&copy; 2022 Blue Cup</footer>
-        </body>
+            <li>
+              <a href="index.html">Shirts</a>
+            </li>
+          </ul>
+        </aside>
+        <main className="main">
+          <Route> // Routes? 
+              <Route path="/" element={<Home />} />
+              <Route path="/catalog" element={<Products />} />
+          </Route>
+        </main>
+        <footer className="footer">&copy; 2022 Blue Cup</footer>
+      </body>
       </div>
-    </Router>
+      </Router>
   );
 }
 
