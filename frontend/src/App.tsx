0133718c6f9c20a97d2516ede7fbe01c2fe5bba5
Cyrus Young <young.2678@buckeyemail.osu.edu>
2022-03-05 10:54:24 -0500

    pages 23-25

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import data from './Products/data';
 
 function App() {
 
@@ -11,79 +12,25 @@ function App() {
     document.querySelector(".sidebar")?.classList.remove("open");
   }
 
-  return (
-    <div className="grid-container">
-      <head>
-        <link rel="stylesheet" href="style.css" />
-        <title>Jet Piranha</title>
-        </head>
-<body>
-        <header className="header">
-            <div className="brand">
-            <button onClick={openMenu}>&#9776;</button>
-            <a href="index.htm">Jet Piranha</a>
+<div className="content">
+  <ul className="products">
+    {
+      data.products.map(product=>
+        <li>
+          <div className="product">
+            <img className = "product-image" src={product.imageURL} alt ="product"/>
+            <div className="product-name">
+              <a href="product.html">{product.name}</a>
+            </div>
+            <div className="product-brand">{product.brand}</div>
+            <div className="product-price">{product.price}</div>
+            <div className="product-rating">{product.rating} Stars ({product.numberOfReviews} Reviews)</div>
           </div>
-          <div className="header-links">
-            <a href="cart.html">Cart</a>
-            <a href="signin.html">Sign In</a>
-          </div>
-        </header>
-        <aside className="sidebar">
-          <h3>Shopping Categories</h3>
-          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-          <ul>
-            <li>
-              <a href="index.html">Pants</a>
-            </li>
-
-            <li>
-              <a href="index.html">Shirts</a>
-            </li>
-          </ul>
-        </aside>
-        <main className="main">
-          <div className="content">
-            <ul className="products">
-              <li>
-                <div className="product">
-                  <img className="product-image" src="images/d1.jpg" alt="product" />
-                  <div className="product-name">
-                    <a href="product.html">Slim Shirt</a>
-                  </div>
-                  <div className="product-brand">Nike</div>
-                  <div className="product-price">$60</div>
-                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                </div>
-              </li>
-              <li>
-                <div className="product">
-                  <img className="product-image" src="images/d1.jpg" alt="product" />
-                  <div className="product-name">
-                    <a href="product.html">Slim Shirt</a>
-                  </div>
-                  <div className="product-brand">Nike</div>
-                  <div className="product-price">$60</div>
-                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                </div>
-              </li>
-              <li>
-                <div className="product">
-                  <img className="product-image" src="images/d1.jpg" alt="product" />
-                  <div className="product-name">
-                    <a href="product.html">Slim Shirt</a>
-                  </div>
-                  <div className="product-brand">Nike</div>
-                  <div className="product-price">$60</div>
-                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </main>
-        <footer className="footer">&copy; 2021 Jet Piranha</footer>
-      </body>
-    </div>
-  );
+        </li>
+        )
+    }
+  </ul>
+</div>
 }
 
 export default App;
